607d4d97bf3138bc7d7427dc6dec222bd431f418
vyarovuy <vyarovuy@twinfield.com>
2015-05-29 18:26:23 +0300

    - create tab with console

--- src/com/github/vitaliyyarovuy/ChutzpahRun.java
+++ src/com/github/vitaliyyarovuy/ChutzpahRun.java
@@ -5,12 +5,16 @@ import com.github.vitaliyyarovuy.runner.ChutzpahRunConfiguration;
 import com.intellij.execution.*;
 import com.intellij.execution.configurations.*;
 import com.intellij.execution.executors.DefaultRunExecutor;
+import com.intellij.execution.filters.TextConsoleBuilder;
+import com.intellij.execution.filters.TextConsoleBuilderFactory;
 import com.intellij.execution.process.*;
 import com.intellij.execution.runners.ExecutionEnvironment;
 import com.intellij.execution.runners.ProgramRunner;
 import com.intellij.execution.testframework.TestConsoleProperties;
 import com.intellij.execution.testframework.sm.runner.SMTRunnerConsoleProperties;
 import com.intellij.execution.testframework.sm.runner.ui.SMTRunnerConsoleView;
+import com.intellij.execution.ui.ConsoleView;
+import com.intellij.execution.ui.ConsoleViewContentType;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.application.ModalityState;
 import com.intellij.openapi.options.SettingsEditor;
@@ -46,6 +50,9 @@ public class ChutzpahRun {
         final List<Exception> exceptions = new ArrayList<Exception>();
         final Ref<Boolean> result = new Ref<Boolean>(true);
 
+        final ChutzpahConsoleToolWindow consoleToolWindow = new ChutzpahConsoleToolWindow(project);
+        consoleToolWindow.setAvailable(true);
+
         ApplicationManager.getApplication().executeOnPooledThread(new Runnable() {
             @Override
             public void run() {
@@ -53,32 +60,23 @@ public class ChutzpahRun {
                 try {
 
                     String tabDisplayName = "chutzpah.console run";
-                    ExecutionHelper.ErrorViewPanel errorTreeView = new ExecutionHelper.ErrorViewPanel(project);
 
-                    ChutzpahConfigurationType type = new ChutzpahConfigurationType();
-
-                    ChutzpahRunConfiguration runConfiguration = new ChutzpahRunConfiguration(project, type
-                            .getConfigurationFactories()[0] , "some label");
-                    ///////////////////////////
-
-                    TestConsoleProperties consoleProperties = new SMTRunnerConsoleProperties(runConfiguration,
-                            "CHUTZPAH", new DefaultRunExecutor());
-                    ///////////////////
-                    SMTRunnerConsoleView  consoleView = new SMTRunnerConsoleView(consoleProperties, new ExecutionEnvironment());
 
                     final GeneralCommandLine commandLine = new GeneralCommandLine(commands);
 
                     Process process = commandLine.createProcess();
                     commandLine.setPassParentEnvironment(true);
                     OSProcessHandler processHandler = new ColoredProcessHandler(process, commandLine.getCommandLineString(), Charsets.UTF_8);
-                    consoleView.attachToProcess(processHandler);
+                    consoleToolWindow.attachToProcess(processHandler);
                     //final ProcessOutput output = new ProcessOutput();
 //                    processHandler.addProcessListener(new ProcessAdapter() {
 //                        @Override
 //                        public void onTextAvailable(ProcessEvent event, Key outputType) {
+//                            ConsoleViewContentType contentType = ConsoleViewContentType.getConsoleViewType(outputType);
+//                            consoleView.print(event.getText(), contentType);
 //                            if (outputType == ProcessOutputTypes.STDERR) {
-//                                output.appendStderr(event.getText());
-//                                ExecutionHelper.showOutput(project, output, "run chutzpah.console", null, true);
+//                                //output.appendStderr(event.getText());
+//                                //ExecutionHelper.showOutput(project, output, "run chutzpah.console", null, true);
 //                            }
 //                            else if (outputType != ProcessOutputTypes.SYSTEM) {
 //                                output.appendStdout(event.getText());

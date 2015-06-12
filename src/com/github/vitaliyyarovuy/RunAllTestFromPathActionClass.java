90f9fdb0d28dc412dc88f1635bf151302de6271f
vyarovuy <vyarovuy@twinfield.com>
2015-06-12 10:35:13 +0300

    add run one file in console

--- src/com/github/vitaliyyarovuy/RunAllTestFromPathActionClass.java
+++ src/com/github/vitaliyyarovuy/RunAllTestFromPathActionClass.java
@@ -5,6 +5,8 @@ import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiDirectory;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
 
 /**
  * Created by vyarovuy on 25.05.2015.
@@ -12,12 +14,20 @@ import com.intellij.psi.PsiDirectory;
 public class RunAllTestFromPathActionClass extends AnAction {
     public void actionPerformed(AnActionEvent event) {
         Project project = event.getData(PlatformDataKeys.PROJECT);
-        PsiDirectory dir = (PsiDirectory)event.getData(PlatformDataKeys.PSI_ELEMENT);
-        assert dir != null;
-        String filePath = dir.getVirtualFile().getPath();
+        PsiElement element = event.getData(PlatformDataKeys.PSI_ELEMENT);
+        assert  element != null;
 
-        ChutzpahRun runTask = new ChutzpahRun();
-        runTask.runAllTestsFromPath(project, filePath);
+        String filePath = null;
+        if(element instanceof PsiDirectory){
+            filePath = ((PsiDirectory)element).getVirtualFile().getPath();
+        } else if(element instanceof PsiFile){
+            filePath = ((PsiFile)element).getVirtualFile().getPath();
+        }
+
+        if(filePath != null){
+            ChutzpahRun runTask = new ChutzpahRun();
+            runTask.runAllTestsFromPath(project, filePath);
+        }
     }
 
 }
